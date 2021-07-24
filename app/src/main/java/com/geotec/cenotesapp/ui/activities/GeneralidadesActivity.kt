db421ad023aa7ef34c18700dad53cfcd7791c122
Andrés Martínez González <andres.geotec@gmail.com>
2021-07-23 23:00:24 -0500

    creación de activities planeados

---
+++ app/src/main/java/com/geotec/cenotesapp/ui/activities/GeneralidadesActivity.kt
@@ -0,0 +1,11 @@
+package com.geotec.cenotesapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeneralidadesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generalidades)
+    }
+}
