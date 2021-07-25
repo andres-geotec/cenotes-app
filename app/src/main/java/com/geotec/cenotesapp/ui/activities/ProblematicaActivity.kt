8d576dbaba9fa4bbd377bdc49f24262f455d863d
Andrés Martínez González <andres.geotec@gmail.com>
2021-07-25 08:55:38 -0500

    formulario: pronblemáticas

--- app/src/main/java/com/geotec/cenotesapp/ui/activities/ProblematicaActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/activities/ProblematicaActivity.kt
@@ -10,8 +10,8 @@ class ProblematicaActivity : AppCompatActivity() {
         setContentView(R.layout.activity_problematica)
 
         val actionbar = getSupportActionBar()
-        // si es una nueva captura mostrar: "Nuevo cenote"
-        actionbar?.setTitle(R.string.cenote_name)
-        actionbar?.setSubtitle(R.string.cenite_id)
+        actionbar?.setTitle(R.string.sec_problematica_title)
+        actionbar?.setSubtitle(R.string.cenote_name)
+        actionbar?.setHomeAsUpIndicator(R.drawable.ic_native_close)
     }
 }
