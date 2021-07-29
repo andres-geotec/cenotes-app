07d6d730341669460482881071b62919230ff917
Andrés Martínez González <andres.geotec@gmail.com>
2021-07-29 02:18:18 -0500

    config: vinculación con vistas activas

--- app/src/main/java/com/geotec/cenotesapp/ui/activities/EditorCenoteActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/activities/EditorCenoteActivity.kt
@@ -4,19 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.geotec.cenotesapp.R
+import com.geotec.cenotesapp.databinding.ActivityEditorCenoteBinding
 
 class EditorCenoteActivity : AppCompatActivity() {
-
+    private lateinit var bv : ActivityEditorCenoteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_editor_cenote)
+        // binding view
+        bv = ActivityEditorCenoteBinding.inflate(layoutInflater)
+        setContentView(bv.root)
 
         val actionbar = getSupportActionBar()
         // si es una nueva captura mostrar: "Nuevo cenote"
         actionbar?.setTitle(R.string.cenote_name_new)
         //actionbar?.setSubtitle(R.string.main_list_title)
+
+        bv.btnShowGeneralDialog.setOnClickListener {
+            Toast.makeText(this, "prueba dialog", Toast.LENGTH_SHORT).show()
+
+            // Para ir al dialos se usa navigation de la sigiente manera
+            /**
+             * val bundle = bundleOf(<name> to <object>)
+             * findNavController().navigate(R.id.<fragmentDialog>, bundle)
+             */
+        }
 
         (findViewById(R.id.button_section_generalidades) as Button).setOnClickListener {
             startActivity(Intent(this, GeneralidadesActivity::class.java))
