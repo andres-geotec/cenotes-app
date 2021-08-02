8e5416c18bbe1207675da8bf1278db7b97c3e307
Andrés Martínez González <andres.geotec@gmail.com>
2021-08-02 04:02:03 -0500

    ejemplo de inserción de datos Sqlite

--- app/src/main/java/com/geotec/cenotesapp/ui/inicio/MainActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/inicio/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.geotec.cenotesapp.ui.inicio
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.geotec.cenotesapp.R
 import com.geotec.cenotesapp.databinding.ActivityMainBinding
+import com.geotec.cenotesapp.sqlite.AdminSQLiteOpenHelper
+import com.geotec.cenotesapp.sqlite.CenoteReaderContract.CenoteEntry
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bv : ActivityMainBinding
@@ -18,8 +21,18 @@ class MainActivity : AppCompatActivity() {
         fragmentTransition.add(R.id.fragmentToolbarMain, ToolbarMainFragment())
         fragmentTransition.commit()
 
+        val dbHelper = AdminSQLiteOpenHelper(this@MainActivity)
+
         bv.buttonTest.setOnClickListener {
-            Toast.makeText(this@MainActivity, R.string.appName, Toast.LENGTH_SHORT).show()
+            val values = ContentValues().apply {
+                put(CenoteEntry.COLUMN_NAME_CLAVE, "0")
+                put(CenoteEntry.COLUMN_NAME_NAME, "Cenote de prueba")
+            }
+
+            val db = dbHelper.writableDatabase
+            val newRowId = db?.insert(CenoteEntry.TABLE_NAME, null, values)
+
+            Toast.makeText(this@MainActivity, "Saved " + newRowId.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
