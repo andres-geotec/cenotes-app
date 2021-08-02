c33ed31ba183cc1a282f5631450877c40946eb43
Andrés Martínez González <andres.geotec@gmail.com>
2021-08-02 01:44:01 -0500

    diseño y adicion de tollbarMainFragment

--- app/src/main/java/com/geotec/cenotesapp/ui/inicio/MainActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/inicio/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import com.geotec.cenotesapp.R
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentTransition = supportFragmentManager.beginTransaction()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentTransition.add(R.id.fragmentToolbarMain, ToolbarMainFragment())
+        fragmentTransition.commit()
     }
 }
