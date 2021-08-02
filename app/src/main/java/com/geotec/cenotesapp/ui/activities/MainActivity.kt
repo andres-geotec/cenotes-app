5032723f7f65f7d3a9989a97ea7cabd6a6044a75
Andrés Martínez González <andres.geotec@gmail.com>
2021-08-01 22:31:38 -0500

    estilo de toolbar main

--- app/src/main/java/com/geotec/cenotesapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/activities/MainActivity.kt
@@ -3,13 +3,15 @@ package com.geotec.cenotesapp.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.geotec.cenotesapp.R
 import com.geotec.cenotesapp.databinding.ActivityMainBinding
 import com.geotec.cenotesapp.ui.editor.EditorActivity
+import com.geotec.cenotesapp.view.iu.fragments.ToolbarMainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bv : ActivityMainBinding
+    private val fragmentManager = supportFragmentManager
+    private val fragmentTransition = fragmentManager.beginTransaction()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +19,22 @@ class MainActivity : AppCompatActivity() {
         bv = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bv.root)
 
+        fragmentTransition.add(R.id.fragment_toolbar_main, ToolbarMainFragment())
+        fragmentTransition.commit()
+
         bv.btnNewRecord.setOnClickListener {
             startActivity(Intent(this, EditorActivity::class.java))
         }
 
 
-        val button_new_record = findViewById(R.id.button_new_record) as Button
-        button_new_record.setOnClickListener {
-            abrirEdicionCenote()
+        bv.buttonNewRecord.setOnClickListener {
+            openEditionCenote()
         }
     }
 
-    fun abrirEdicionCenote() {
+    private fun openEditionCenote() {
         val intent = Intent(this, EditorCenoteActivity::class.java)
         startActivity(intent)
-        //Toast.makeText(this@MainActivity, "mandar actividad", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this@MainActivity, "send activity", Toast.LENGTH_SHORT).show()
     }
 }
