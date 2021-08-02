02645954c55244b377cc0476c7408e9dd3e7ffd5
Andrés Martínez González <andres.geotec@gmail.com>
2021-08-02 01:08:47 -0500

    colores y estilo por defecto

--- app/src/main/java/com/geotec/cenotesapp/ui/inicio/MainActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/inicio/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.geotec.cenotesapp
+package com.geotec.cenotesapp.ui.inicio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geotec.cenotesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
