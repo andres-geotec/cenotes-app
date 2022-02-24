4d5fcb77d2e9e52225a06a1341d51343df508110
Andrés Martínez González <andres.geotec@gmail.com>
2022-02-24 11:57:43 -0600

    navegación a lista de secciones del cenote

--- app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteSectionListFragment.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteSectionListFragment.kt
@@ -1,13 +1,11 @@
-package com.geotec.cenotesapp.ui.cenotes
+package com.geotec.cenotesapp.ui.cenotes.sections
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.geotec.cenotesapp.R
-import com.geotec.cenotesapp.databinding.FragmentCenoteListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,14 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CenoteListFragment.newInstance] factory method to
+ * Use the [CenoteSectionListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CenoteListFragment : Fragment() {
-    // variable para acceder al contenido de las vistas
-    private var _v: FragmentCenoteListBinding? = null
-    private val v get() = _v!!
-
+class CenoteSectionListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -39,24 +33,9 @@ class CenoteListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        _v = FragmentCenoteListBinding.inflate(inflater, container, false)
-        return v.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        v.btnCreateNewCenote.setOnClickListener {
-            toCenoteSectionsFragment()
-        }
-    }
-
-    private fun toCenoteSectionsFragment() {
-        findNavController().navigate(
-            R.id.action_cenoteListFragment_to_cenoteSectionListFragment
-        )
+        return inflater.inflate(R.layout.fragment_cenote_section_list, container, false)
     }
 
     companion object {
@@ -66,12 +45,12 @@ class CenoteListFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CenoteListFragment.
+         * @return A new instance of fragment CenoteSectionListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CenoteListFragment().apply {
+            CenoteSectionListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
