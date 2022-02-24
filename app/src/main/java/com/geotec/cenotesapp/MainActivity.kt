1ed03d1994f6f3cca72e9af3e39bcec7fa100fa9
Andrés Martínez González <andres.geotec@gmail.com>
2022-02-23 23:09:08 -0600

    ejemplo de vistas activas en MainActivity

--- app/src/main/java/com/geotec/cenotesapp/MainActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.geotec.cenotesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.geotec.cenotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // variable para acceder al contenido de las vistas
+    private lateinit var v: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        v = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(v.root)
+
+        v.buttonTest.setOnClickListener {
+            Toast.makeText(this@MainActivity, R.string.app_name, Toast.LENGTH_SHORT).show()
+        }
     }
 }
