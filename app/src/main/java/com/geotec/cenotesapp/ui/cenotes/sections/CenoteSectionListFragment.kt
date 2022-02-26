d37403d4383e4d5dc4600a352c2bb3f10fcae4c5
Andrés Martínez González <andres.geotec@gmail.com>
2022-02-26 00:40:20 -0600

    vista de sección Clasificación

--- app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteSectionListFragment.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteSectionListFragment.kt
@@ -110,8 +110,8 @@ class CenoteSectionListFragment : Fragment(), CenoteSectionListListener {
         ))
         list.add(CenoteSection(
             getString(R.string.cenote_section_clasifi_title),
-            R.id.action_cenoteSectionListFragment_to_cenoteSectionGeneralFragment,
-            false,
+            R.id.action_cenoteSectionListFragment_to_cenoteSectionClasifiFragment,
+            true,
             0,
             1
         ))
