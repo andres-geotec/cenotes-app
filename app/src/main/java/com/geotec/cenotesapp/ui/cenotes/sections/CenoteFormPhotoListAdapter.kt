47eca32645f3d21518d8e06b99e3161aa972eae7
Andrés Martínez González <andres.geotec@gmail.com>
2022-02-27 16:36:48 -0600

    card foto con input de descripción

---
+++ app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteFormPhotoListAdapter.kt
@@ -0,0 +1,18 @@
+package com.geotec.cenotesapp.ui.cenotes.sections
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.geotec.cenotesapp.databinding.ItemCenoteFormPhotoBinding
+
+class CenoteFormPhotoListAdapter: RecyclerView.Adapter<CenoteFormPhotoListAdapter.ViewHolder>() {
+    inner class ViewHolder(v: ItemCenoteFormPhotoBinding): RecyclerView.ViewHolder(v.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        ItemCenoteFormPhotoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
+
+    override fun getItemCount(): Int = 1
+}
