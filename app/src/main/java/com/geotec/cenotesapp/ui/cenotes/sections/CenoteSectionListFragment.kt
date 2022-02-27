d26c521943a9b8a17e11f7b60fd8a77676824bb3
Andrés Martínez González <andres.geotec@gmail.com>
2022-02-26 21:09:51 -0600

    vista de sección Gestion

--- app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteSectionListFragment.kt
+++ app/src/main/java/com/geotec/cenotesapp/ui/cenotes/sections/CenoteSectionListFragment.kt
@@ -138,8 +138,8 @@ class CenoteSectionListFragment : Fragment(), CenoteSectionListListener {
         ))
         list.add(CenoteSection(
             getString(R.string.cenote_section_gestion_title),
-            R.id.action_cenoteSectionListFragment_to_cenoteSectionGeneralFragment,
-            false,
+            R.id.action_cenoteSectionListFragment_to_cenoteSectionGestionFragment,
+            true,
             0,
             1
         ))
