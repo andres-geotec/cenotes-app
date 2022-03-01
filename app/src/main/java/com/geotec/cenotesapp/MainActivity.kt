435b2f2934aa0c3859ab6a9916d2504b5a21d9cd
Andrés Martínez González <andres.geotec@gmail.com>
2022-03-01 11:15:36 -0600

    toolbar principal

--- app/src/main/java/com/geotec/cenotesapp/MainActivity.kt
+++ app/src/main/java/com/geotec/cenotesapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.geotec.cenotesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.geotec.cenotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         v = ActivityMainBinding.inflate(layoutInflater)
         setContentView(v.root)
+
+        setSupportActionBar(v.tbrMain)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_popup_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mnuMainOpAbout -> Toast.makeText(
+                this,
+                getString(R.string.menu_main_op_about),
+                Toast.LENGTH_SHORT
+            ).show()
+            R.id.mnuMainOpSettings -> Toast.makeText(
+                this,
+                getString(R.string.menu_main_op_settings),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
